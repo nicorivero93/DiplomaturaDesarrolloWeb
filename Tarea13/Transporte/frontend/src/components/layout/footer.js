5e5d7c1109ae58f313936f4c48a587c00070bbe7
Tomé Rivero Lucas <ltome@grupost.com.ar>
2024-09-13 21:38:20 -0300

    sbuida de tarea13 con react

---
+++ Tarea13/Transporte/frontend/src/components/layout/footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = (props) => {
+    return (
+        <footer>
+            <p> Derechos reservados. Transpor X 2024 </p>
+        </footer>
+    );
+}
+
+export default Footer;
